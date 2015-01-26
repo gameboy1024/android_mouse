cb046f0a093d380cda49d710b35799ac131c45df
gameboy1024 <gameboy1024@hotmail.com>
2015-01-26 14:52:45 +0100

    Forgot a file

---
+++ app/src/main/java/com/sunbotu/androidmouse/joystick/JoystickClickListener.java
@@ -0,0 +1,10 @@
+package com.sunbotu.androidmouse.joystick;
+
+/**
+ * Created by sbt on 1/16/15.
+ */
+public interface JoystickClickListener {
+    public void OnClicked();
+
+    public void OnReleased();
+}
